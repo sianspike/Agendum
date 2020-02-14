ae57e3c98b36fc78879afdedaf38b3b4e1fe433a
Sian Pike <sian.pike@icloud.com>
2020-02-14 10:04:29 +0000

    sorting file location issues

--- Agendum/SignUpView.swift
+++ Agendum/SignUpView.swift
@@ -9,8 +9,45 @@
 import SwiftUI
 
 struct SignUpView: View {
+    @State var username: String = ""
+    @State var email: String = ""
+    @State var password: String = ""
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Spacer()
+            Text("Sign Up")
+                .font(Font.custom("Montserrat-Medium", size: 30))
+                .foregroundColor(Color(red: 0.6, green: 0.9, blue: 1.0, opacity: 1.0))
+            TextFieldWithBottomBorder(placeholder: "Username", text: username)
+            TextFieldWithBottomBorder(placeholder: "Email", text: email)
+            TextFieldWithBottomBorder(placeholder: "Password", text: password)
+            Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
+                Text("CREATE ACCOUNT")
+                    .frame(minWidth: 0, maxWidth: .infinity)
+                    .font(Font.custom("Monsterrat-Medium", size: 15))
+                    .foregroundColor(.black)
+                    .padding(10)
+                    .background(Color(red: 0.6, green: 0.8, blue: 1.0, opacity: 1.0))
+            }.padding(.bottom)
+            Button(action: {}) {
+                Text("SIGN UP WITH FACEBOOK")
+                    .frame(minWidth: 0, maxWidth: .infinity)
+                    .font(Font.custom("Monsterrat-Medium", size: 15))
+                    .foregroundColor(.black)
+                    .padding(10)
+                    .background(Color(red: 0.6, green: 0.8, blue: 1.0, opacity: 1.0))
+            }
+            Spacer()
+            Button(action: {}) {
+                Text("SIGN IN")
+                    .frame(minWidth: 0, maxWidth: .infinity)
+                    .font(Font.custom("Monsterrat-Medium", size: 15))
+                    .foregroundColor(.black)
+                    .padding(10)
+                    .background(Color(red: 0.6, green: 1.0, blue: 0.8, opacity: 1.0))
+            }
+        }.padding()
     }
 }
 
