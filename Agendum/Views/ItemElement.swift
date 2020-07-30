f0d61affa2ae289697f85e98bb534e8525aafa6d
Sian Pike <sian.pike@icloud.com>
2020-07-30 21:02:47 +0100

    Replaced itemelements with simpletaskview

--- Agendum/Views/ItemElement.swift
+++ Agendum/Views/ItemElement.swift
@@ -7,6 +7,7 @@
 //
 
 import SwiftUI
+import CareKitUI
 
 struct ItemElement: View {
     
@@ -14,74 +15,128 @@ struct ItemElement: View {
     @State var dragOffset = CGSize.zero
     @EnvironmentObject var session: FirebaseSession
     
+    func thing(){
+        
+        self.item.toggleCompleted()
+        
+        if (self.item.isCompleted()) {
+        
+            self.session.loggedInUser!.progress += 1
+        
+        } else {
+        
+            self.session.loggedInUser!.progress -= 1
+        }
+        
+        self.session.saveProgress(progress: self.session.loggedInUser!.progress)
+        self.session.saveItem(item: self.item)
+    }
+    
     var body: some View {
         
-        ZStack {
-                    
-            HStack{
-                            
-                Text(String(item.getTitle()))
-                    .strikethrough(self.item.isCompleted() ? true : false, color: Color(.gray))
-                    .background(Color(.white))
-                    .foregroundColor(self.item.isCompleted() ? Color(.gray) : Color(.black))
-                    .frame(alignment: .leading)
-                            
-                Spacer()
-                                    
-                Button(action: {
-                                        
-                    self.item.toggleCompleted()
-                                
-                    if (self.item.isCompleted()) {
-                                    
-                        self.session.loggedInUser!.progress += 1
-                                    
-                    } else {
-                                    
-                        self.session.loggedInUser!.progress -= 1
-                    }
-                                
-                    self.session.saveProgress(progress: self.session.loggedInUser!.progress)
-                    self.session.saveItem(item: self.item)
-                                        
-                }) {
-                                        
-                    Image(uiImage: UIImage(named: self.item.isCompleted() ? "Icons/Complete Tick.png" : "Icons/InComplete Tick.png")!)
-                        .resizable()
-                        .renderingMode(.original)
-                        .shadow(color: Color.black.opacity(0.2), radius: 2, x: 2, y: 2)
-                        .frame(width: 30, height: 30)
-                                        
-                }
-                    .frame(alignment: .trailing)
-                    .background(Color(.white))
-                            
-            }
-                .frame(maxWidth: .infinity, alignment: .leading)
+        
+        //DRAG GESTURE NO LONGER WORKS
+        ZStack{
+            
+            HStack {
+                
+                SimpleTaskView(title: Text(item.getTitle())
+                                        .strikethrough(self.item.isCompleted() ? true : false, color: Color(.gray))
+                                        .foregroundColor(self.item.isCompleted() ? Color(.gray) : Color(.black)),
+                                detail: nil, isComplete: item.isCompleted(), action: thing).accentColor(Color(red: 0.6, green: 0.9, blue: 1.0, opacity: 1.0))
+            }.frame(maxWidth: .infinity, alignment: .leading)
                 .background(Color(.white))
                 .offset(x: self.dragOffset.width)
-                    .gesture(DragGesture()
-                        .onChanged { value in
-                                    
-                            self.dragOffset = value.translation
+                .gesture(DragGesture()
+                    .onChanged { value in
+            
+                        self.dragOffset = value.translation
+                    }
+                    .onEnded { value in
+            
+                        if (self.dragOffset.width < 0) {
+            
+                            self.session.deleteItem(item: self.item)
+                            self.session.retrieveItems()
+            
+                        } else if (self.dragOffset.width > 0) {
+            
+                            //edit item
                         }
-                        .onEnded { value in
-                            
-                            if (self.dragOffset.width < 0) {
-                                
-                                self.session.deleteItem(item: self.item)
-                                self.session.retrieveItems()
-                                
-                            } else if (self.dragOffset.width > 0) {
-                                
-                                //edit item
-                            }
-                            
-                            self.dragOffset = .zero
-                        })
-                    
-        }
-            .frame(maxWidth: .infinity)
+            
+                        self.dragOffset = .zero
+                    })
+            
+
+        }.frame(maxWidth: .infinity)
             .background(Color(red: 0.6, green: 1.0, blue: 0.8, opacity: 1.0))
+        
+//        ZStack {
+//
+//            HStack{
+//
+//                Text(String(item.getTitle()))
+//                    .strikethrough(self.item.isCompleted() ? true : false, color: Color(.gray))
+//                    .background(Color(.white))
+//                    .foregroundColor(self.item.isCompleted() ? Color(.gray) : Color(.black))
+//                    .frame(alignment: .leading)
+//
+//                Spacer()
+//
+//                Button(action: {
+//
+//                    self.item.toggleCompleted()
+//
+//                    if (self.item.isCompleted()) {
+//
+//                        self.session.loggedInUser!.progress += 1
+//
+//                    } else {
+//
+//                        self.session.loggedInUser!.progress -= 1
+//                    }
+//
+//                    self.session.saveProgress(progress: self.session.loggedInUser!.progress)
+//                    self.session.saveItem(item: self.item)
+//
+//                }) {
+//
+//                    Image(uiImage: UIImage(named: self.item.isCompleted() ? "Icons/Complete Tick.png" : "Icons/InComplete Tick.png")!)
+//                        .resizable()
+//                        .renderingMode(.original)
+//                        .shadow(color: Color.black.opacity(0.2), radius: 2, x: 2, y: 2)
+//                        .frame(width: 30, height: 30)
+//
+//                }
+//                    .frame(alignment: .trailing)
+//                    .background(Color(.white))
+//
+//            }
+//                .frame(maxWidth: .infinity, alignment: .leading)
+//                .background(Color(.white))
+//                .offset(x: self.dragOffset.width)
+//                    .gesture(DragGesture()
+//                        .onChanged { value in
+//
+//                            self.dragOffset = value.translation
+//                        }
+//                        .onEnded { value in
+//
+//                            if (self.dragOffset.width < 0) {
+//
+//                                self.session.deleteItem(item: self.item)
+//                                self.session.retrieveItems()
+//
+//                            } else if (self.dragOffset.width > 0) {
+//
+//                                //edit item
+//                            }
+//
+//                            self.dragOffset = .zero
+//                        })
+//
+//        }
+//            .frame(maxWidth: .infinity)
+//            .background(Color(red: 0.6, green: 1.0, blue: 0.8, opacity: 1.0))
     }
 }
